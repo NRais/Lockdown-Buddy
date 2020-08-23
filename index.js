4999115dc00df8759fad145f83ee571872e735ea
CrunchyPancakes <30matthewt@gmail.com>
2020-08-23 14:31:02 +1200

    Setting up Heroku

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express'); // make sure we're using express!
 const app = express(); // make our 'app' an express one. 
 const port = process.env.PORT || 3000 // Port that the server will run on. Will use a default available one, otherwise 3000.
 
-app.get('/', (req, res) => res.send(`Lockdown Avengers Assemble!`))
-app.get('/ping', (req, res) => res.send('pong'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/html/frame0.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
